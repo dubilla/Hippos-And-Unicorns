07918e5b736801db2c797d3369b81928e5d03221
Dan Ubilla <dan.ubilla@gmail.com>
2014-03-01 14:40:31 -0500

    Randomize animal queried, randomize image, and set total images possible per animal

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,9 @@ angular.module('hipposAndUnicornsApp')
   .controller('MainCtrl', function ($scope, Animal) {
     Animal.getAnimal()
       .then(function(data) {
-        $scope.imageSrc = data.data.responseData.results[0].url
+        var images = data.data.responseData.results,
+          randomImage = images[Math.floor(Math.random() * images.length)];
+
+        $scope.imageSrc = randomImage.url;
       });
   });
--- app/scripts/services/animals.js
+++ app/scripts/services/animals.js
@@ -3,15 +3,21 @@
 angular.module('hipposAndUnicornsApp')
   .provider('Animal', function () {
     this.$get = function ($http) {
-      var service = {
+      var animals = ['hippo', 'unicorn'],
+          randomAnimal = animals[Math.floor(Math.random()*animals.length)],
+          service;
+
+      service = {
         getAnimal: function () {
           return $http({
             method: 'JSONP',
             url: 'http://ajax.googleapis.com/ajax/services/search/images',
             params: {
               v: '1.0',
-              q: 'unicorn',
-              callback: 'JSON_CALLBACK'
+              q: randomAnimal,
+              safe: 'active',
+              callback: 'JSON_CALLBACK',
+              start: Math.floor(Math.random() * 20)
             }
           });
         }
